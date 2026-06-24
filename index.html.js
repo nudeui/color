52d3b52fe223ee8205df7fbceed6c8bc54117bad
Lea Verou <lea@verou.me>
2026-06-25 00:45:52 +0300

    Persist color scheme switch

--- index.html.js
+++ index.html.js
@@ -12,9 +12,13 @@ export default html`
 		</head>
 		<body>
 			<label>
-				<input type="checkbox" />
-				Dark mode
+				<input type="checkbox" id="invert-color-scheme" onchange="localStorage.invertColorScheme = this.checked" />
+				Invert color scheme
 			</label>
+			<script>
+				// Restore persisted dark mode state right away to avoid a flash
+				document.getElementById("invert-color-scheme").checked = localStorage.invertColorScheme === "true";
+			</script>
 			<table>
 				<thead>
 					<tr>
