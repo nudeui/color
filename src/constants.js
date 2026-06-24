4b0a2d41a3165d6fe9136c334846d7ed82fde3fb
Lea Verou <lea@verou.me>
2026-06-24 15:35:24 +0300

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -31,7 +31,7 @@ export const colors = {
 	cyan: "oklch(70% 0.155 205)",
 	blue: "oklch(55% 0.23 255)",
 	indigo: "oklch(52% 0.178 268)",
-	pink: "oklch(60% 0.28 355)",
+	pink: "oklch(60% 0.28 354)",
 	red: "oklch(59% 0.21 20)",
 	gray: "oklch(50% 0.03 270)",
 }
