8b85d9aeb24ca232cd9c321798fbe3b4dfc73427
Lea Verou <lea@verou.me>
2026-06-25 13:28:01 +0300

    Prettier

--- src/util.js
+++ src/util.js
@@ -1,9 +1,9 @@
 import { round, clamp, progress, calc, ife } from "airdry/css";
 
-export function snapToScale(x, points, { strat = '', indent = "\t" } = {}) {
+export function snapToScale (x, points, { strat = "" } = {}) {
 	const terms = points.slice(1).map((b, i) => {
 		const a = points[i];
-		return `(${b} - ${a}) * ${round(strat, clamp(0, progress(x, a, b), 1) )}`;
+		return `(${b} - ${a}) * ${round(strat, clamp(0, progress(x, a, b), 1))}`;
 	});
 	terms.unshift(points[0]);
 
