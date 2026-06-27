92e13a33488edd62bd13776bdc6cb503b5576f10
Lea Verou <lea@verou.me>
2026-06-28 01:14:47 +0300

    Update index.html.js

--- index.html.js
+++ index.html.js
@@ -1,72 +1,74 @@
 import html from "airdry/html";
 import { colors, levelsChromatic, hues } from "./src/index.js";
 
+export { template } from "airdry/html";
+
+export const head = `<link rel="stylesheet" href="demo.css" />`;
+export const title = "Nude color demo";
+
+const callout = ({ hue, buttons }, ...contents) => {
+	return html`
+		<article class="callout" style="${hue ? `--color: var(--color-${hue})` : ""}">
+			<h3>${hue ?? "Callout"}</h3>
+			${hue ? html`<p>Color: <code>var(--color-${hue})</code></p>` : ""}
+			${contents.join("\n")}
+			${buttons
+				? html`<footer>
+						<button>Primary</button><button class="outline">Secondary</button>
+					</footer>`
+				: ""}
+		</article>
+	`;
+};
+
 export default html`
-	<!DOCTYPE html>
-	<html lang="en">
-		<head>
-			<meta charset="UTF-8" />
-			<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-			<meta name="color-scheme" content="light dark" />
-			<title>Test</title>
-			<link rel="stylesheet" href="demo.css" />
-		</head>
-		<body>
-			<label>
-				<input
-					type="checkbox"
-					id="invert-color-scheme"
-					onchange="localStorage.invertColorScheme = this.checked"
-				/>
-				Invert color scheme
-			</label>
-			<script>
-				// Restore persisted dark mode state right away to avoid a flash
-				{
-					let control = document.getElementById("invert-color-scheme");
-					if (localStorage.invertColorScheme === "true") {
-						control.checked = true;
-					}
-				}
-			</script>
-			<table>
-				<thead>
-					<tr>
-						<th>Hue</th>
-						<th>Key</th>
-						${levelsChromatic.map(level => html`<th>${level}</th>`)}
+	<label>
+		<input
+			type="checkbox"
+			id="invert-color-scheme"
+			onchange="localStorage.invertColorScheme = this.checked"
+		/>
+		Invert color scheme
+	</label>
+	<script>
+		// Restore persisted dark mode state right away to avoid a flash
+		{
+			let control = document.getElementById("invert-color-scheme");
+			if (localStorage.invertColorScheme === "true") {
+				control.checked = true;
+			}
+		}
+	</script>
+	<table>
+		<thead>
+			<tr>
+				<th>Hue</th>
+				<th>Key</th>
+				${levelsChromatic.map(level => html`<th>${level}</th>`)}
+			</tr>
+		</thead>
+		<tbody>
+			${hues.map(
+				hue => html`
+					<tr style="--color: var(--color-${hue})">
+						<th>${hue}</th>
+						<th class="swatch"></th>
+						${levelsChromatic.map(level => ({
+							tag: "td",
+							class: "swatch",
+							style: `--tint: var(--tint-${level})`,
+						}))}
 					</tr>
-				</thead>
-				<tbody>
-					${hues.map(
-						hue => html`
-							<tr style="--color: var(--color-${hue})">
-								<th>${hue}</th>
-								<th class="swatch"></th>
-								${levelsChromatic.map(level => ({
-									tag: "td",
-									class: "swatch",
-									style: `--tint: var(--tint-${level})`,
-								}))}
-							</tr>
-						`,
-					)}
-				</tbody>
-			</table>
-			<div class="callouts">
-				${hues.map(
-					hue => html`
-						<article class="callout" style="--color: var(--color-${hue})">
-							<h3>${hue}</h3>
-							<p>Color: <code>var(--color-${hue})</code></p>
-							<footer>
-								<button>Primary</button>
-								<button class="outline">Secondary</button>
-							</footer>
-						</article>
-					`,
-				)}
-			</div>
-		</body>
-	</html>
+				`,
+			)}
+		</tbody>
+	</table>
+	<div class="callouts">
+		${hues.map(hue =>
+			callout(
+				{ hue, buttons: true },
+				"This is a callout",
+				callout({}, "Nested callout 1", callout({}, "Nested callout 2")),
+			))}
+	</div>
 `;
