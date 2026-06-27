5d818338ce78b09a21f6036804a53970bea7982c
Lea Verou <lea@verou.me>
2026-06-28 01:14:40 +0300

    WIP

--- src/util.js
+++ src/util.js
@@ -1,4 +1,53 @@
-import { round, clamp, progress, calc, ife, sop } from "airdry/css";
+import { css, round, clamp, progress, calc, ife, sop } from "airdry/css";
+import { toKebabCase } from "airdry/lang/util";
+import { levelsChromatic } from "./index.js";
+
+export function template (
+	content,
+	{ selector = "&, :host", start = "", end = "", properties = {} } = {},
+) {
+	return css`
+		${start}
+		${registerProperties(properties)}
+		:where(${selector}) {
+			${content}
+		}
+		${end}
+	`;
+}
+
+const initialValues = {
+	"<color>": "transparent",
+	"<number>": 0,
+	"<integer>": 0,
+	"<percentage>": "0%",
+	"<length>": "0px",
+	"<angle>": "0deg",
+	"<time>": "0s",
+	"<image> | none": "none",
+};
+
+export function registerProperty (name, { syntax = "*", inherits = true, initial } = {}) {
+	name = toKebabCase(name);
+	if (Array.isArray(syntax)) {
+		syntax = syntax.join(" | ");
+	}
+	initial ??= initialValues[syntax];
+
+	return css`
+		@property --${name} {
+			syntax: "${syntax}";
+			inherits: ${Boolean(inherits) + ""};
+			${initial !== undefined ? `initial-value: ${initial};` : ""}
+		}
+	`;
+}
+
+export function registerProperties (properties) {
+	return Object.entries(properties)
+		.map(([name, options]) => registerProperty(name, options))
+		.join("\n");
+}
 
 export function snapToScale (x, points, { strat = "" } = {}) {
 	const terms = points.flatMap((b, i) => {
@@ -9,7 +58,7 @@ export function snapToScale (x, points, { strat = "" } = {}) {
 		return [`${b} - ${a}`, round(strat, progress(x, a, b))];
 	});
 
-	return sop(terms);
+	return sop(...terms);
 }
 
 export function tint_if (condition, tint_true, tint_false) {
@@ -18,4 +67,15 @@ export function tint_if (condition, tint_true, tint_false) {
 			ife(condition, `var(--${c}-${tint_true}, ${c})`, `var(--${c}-${tint_false}, ${c})`))
 		.map(c => calc(c))
 		.join(" ");
+}
+
+export function tint (color, tint) {
+	return `oklch(from ${color} var(--tint-${tint}))`;
+}
+
+export function scale (name, levels = levelsChromatic) {
+	// --color-core-10: oklch(from var(--color-core) var(--tint-10));
+	return levels
+		.map(level => `--color-${name}-${level}: ${tint(`var(--color-${name})`, level)};`)
+		.join("\n");
 }
